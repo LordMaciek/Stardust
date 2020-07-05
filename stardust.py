868e75b3bb9e408b1f9563dceaa63eed6211ac16
LordMaciek <maciek.a.bienkowski@gmail.com>
2020-07-05 23:52:23 +0200

    Basic functionality should be there!

--- stardust.py
+++ stardust.py
@@ -1,16 +1,16 @@
 import crop_scale
-import stack
 import take_picture
-import serve
+
 """This wee app is designed specifically for astrophotography. Taking repeated pix, cropping, stacking and - in the
 futur - sharpening. Finally, it should run a simple server showing you your creation"""
+
+with open("welcome.txt", "r") as f:
+    WELCOME = f.read()
+
+with open("prompt.txt", "r") as f:
+    MENU_PROMPT = f.read()
+
 def main():
-    with open("welcome.txt", "r") as f:
-        WELCOME = f.read()
-
-    with open("prompt.txt", "r") as f:
-        MENU_PROMPT = f.read()
-
     print(WELCOME)
     input(MENU_PROMPT)
     take_picture.main()
