91ed6b12c47bf224f0546e978f26e3a3e0e0a8ab
LordMaciek <maciek.a.bienkowski@gmail.com>
2020-07-05 22:53:31 +0200

    Server repaired

--- serve.py
+++ serve.py
@@ -13,11 +13,15 @@ def get_IP():
     finally:
         return my_ip
 
+# Setting some variables needed for http.server to run,
+# Keeping the default port 8080
 PORT = 8080
 Handler = http.server.SimpleHTTPRequestHandler
 
+# Changing to the directory, where the ready pictures will be.
 os.chdir('pix_ready')
 
+# Running the server
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at {}:8080".format(my_ip))
+    print("serving at {}:8080".format(get_IP()))
     httpd.serve_forever()
