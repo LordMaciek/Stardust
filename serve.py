c54cd7d4e55cf40476291cb1618d124a5d4a669f
LordMaciek <maciek.a.bienkowski@gmail.com>
2020-07-05 22:56:42 +0200

    Server repaired

--- serve.py
+++ serve.py
@@ -6,14 +6,14 @@ import os
 # First, let's find out what our local IP address is, so we can advise the user to it later on.
 def get_IP():
     try:
-        host_name = socke.gethostname()
+        host_name = socket.gethostname()
         my_ip = socket.gethostbyname(host_name)
     except:
         print("Couldn't get my IP address. Please check your network and do try again.")
     finally:
         return my_ip
 
-ip_address = get_IP()
+# ip_address = get_IP()
 
 # Setting some variables needed for http.server to run,
 # Keeping the default port 8080
@@ -25,5 +25,5 @@ os.chdir('pix_ready')
 
 # Running the server
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at {}:8080".format(ip_address))
+    print("serving at {}:8080".format(get_IP))
     httpd.serve_forever()
